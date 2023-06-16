79d314e65e338c1a81f0a674ac615b211f20c7fd
kilimnik <mail@kilimnik.de>
2023-06-17 00:32:17 +0200

    Fix Pickle Support

--- src/betterproto/__init__.py
+++ src/betterproto/__init__.py
@@ -858,6 +858,15 @@ class Message(ABC):
         """
         return bytes(self)
 
+    def __getstate__(self):
+        return bytes(self)
+
+    def __setstate__(self, pickled_bytes):
+        o = self.parse(pickled_bytes)
+        o.__post_init__()
+
+        return o
+
     @classmethod
     def _type_hint(cls, field_name: str) -> Type:
         return cls._type_hints()[field_name]
